2a3b0f0c657d0fb1ec217b9282c7903ae9bb2287
Joshua Garde <jgarde@jpl.nasa.gov>
2023-07-25 18:29:43 -0700

    Linting

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export * as models from './lib/models.js';
-export * as sdsStatuses from './lib/sds-statuses.js'
+export * as sdsStatuses from './lib/sds-statuses.js';
 export * as utilities from './lib/utilities.js';
